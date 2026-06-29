07b5603831e22cbb202dd34b6bce29c504105329
donna-lott <l.kiknadze@proton.me>
2026-06-29 16:42:52 +0200

    ubdated html and java script, so it won't load all the pages at once

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 function openTab(tabName) {
   document.querySelectorAll('.tab-content').forEach(content => {
+    content.classList.remove('active');
     content.style.display = 'none';
   });
+  document.querySelectorAll('.tab-button').forEach(button => {
+    button.classList.remove('active');
+  });
+  document.getElementById(tabName).classList.add('active');
   document.getElementById(tabName).style.display = 'block';
+  event.target.classList.add('active');
 }
