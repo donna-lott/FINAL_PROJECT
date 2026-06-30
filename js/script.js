32c893957c384bc8361a6fe1ee8caad7bc79eddb
donna-lott <l.kiknadze@proton.me>
2026-06-30 08:34:08 +0200

    fix: remove unused logo-white class and related JavaScript toggle

--- js/script.js
+++ js/script.js
@@ -10,5 +10,3 @@ function openTab(tabName) {
   document.getElementById(tabName).style.display = 'block';
   event.target.classList.add('active');
 }
-// This tells JavaScript to add/remove the 'logo-white' class when needed
-document.querySelector('.logo').classList.toggle('logo-white');
